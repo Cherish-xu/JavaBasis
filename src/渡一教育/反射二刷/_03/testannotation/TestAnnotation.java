9f4b28e9178b6dd2531943f5eba5f42bf12089e2
zhangtianxu <1638630731@qq.com>
2020-03-18 08:46:52 +0800

    My first 线程之join 线程死锁之模拟哲学家进餐问题 线程之模拟一个发送垃圾短信的计时器 反射之通过反射获取类、类的属性相关信息 反射之方法、构造方法、操作String练习 反射之IOC初识

--- src/渡一教育/反射二刷/_03/testannotation/TestAnnotation.java
+++ src/渡一教育/反射二刷/_03/testannotation/TestAnnotation.java
@@ -11,8 +11,15 @@ public class TestAnnotation {
 
     public static void main(String[] args) {
         //当前系统的时间
-        Date date = new Date();
-        //已废弃的    @Deprecated
-        date.getYear();
+//        Date date = new Date();
+//        //已废弃的    @Deprecated
+//        date.getYear();
+
+
+        // 注解括号里是一个数组，如果只有一个的话就可以不加大括号
+        //unused -- 变量定义后未被使用
+        @SuppressWarnings("unused")
+        String str = "abc";
+
     }
 }
---
+++ src/渡一教育/反射二刷/_03/testannotation/TestInterface.java
@@ -0,0 +1,10 @@
+package 渡一教育.反射二刷._03.testannotation;
+
+/**
+ * @Author: 张天旭
+ * @Date: 2020/3/18 8:29
+ * @Version 1.0
+ */
+public interface TestInterface {
+    public void test();
+}
